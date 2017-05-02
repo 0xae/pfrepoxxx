ca3384765cf0bb9d2ee420f55c172cbb3134c20f
Ayrton Gomes <com.ayrton@gmail.com>
2017-05-02 14:26:31 -0100

    evento bilhetes submodule added

--- backend/views/evento/evento_bilhetes.php
+++ backend/views/evento/evento_bilhetes.php
@@ -1,63 +1,62 @@
+<?php
+
+use yii\helpers\ArrayHelper;
+$bilhetesLabel = ArrayHelper::map($_dataBilhetes, 'idbilhete', 'nome_bilhete');
+$k=0;
+?>
+
 <div class="col-md-12 titulosection">
 <div class="detail_bilhete">
 <div role="tabpanel">
-
     <ul class="nav nav-tabs bilhte" role="tablist">
-        <li role="presentation" class="active">
-            <a href="#geral" aria-controls="home" role="tab" data-toggle="tab" aria-expanded="true">Geral</a>
-        </li>
-
-        <li role="presentation" class="">
-            <a href="#9" aria-controls="tab" role="tab" data-toggle="tab" aria-expanded="false">Normal </a>
-        </li>
-
-        <li role="presentation" class="">
-            <a href="#10" aria-controls="tab" role="tab" data-toggle="tab" aria-expanded="false">Vip</a>
-        </li>
+        <?php foreach ($bilhetesLabel as $id=>$label): ?>
+        <li role="presentation" <? if (!$k){ $k=$id; echo 'class="active"'; }?>>
+                <a href="#bil_<?= $id ?>" 
+                   aria-controls="home" role="tab" 
+                   data-toggle="tab" aria-expanded="false"><?= $label ?></a>
+            </li>
+        <?php endforeach; ?>
     </ul>
 
     <div class="tab-content">
-        <div role="tabpanel" class="tab-pane fade active in" id="geral">
-            <div class="row">
-                <div class="col-md-6">
-                    <h2 style="color: #313541; font-weight: 700;">Descrição</h2>
-                    <p style="font-size: 17px;color: #313541;">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                    <h2 style="color: #313541; font-weight: 700;">Preço</h2>
-                    <h1 style="color: #009447; font-weight: 700; margin-top: -10px;">500ECV</h1>
-                </div>
+        <?php foreach ($_dataBilhetes as $b): ?>
+            <div role="tabpanel" class="tab-pane fade <? if ($b->idbilhete==$k) echo 'active'; ?> in" id="bil_<?= $b->idbilhete ?>">
+                <div class="row">
+                    <div class="col-md-6">
+                        <h2 style="color: #313541; font-weight: 700;">Descrição</h2>
+                        <p style="font-size: 17px;color: #313541;"> <?= $b->descricao_bilhete; ?> </p>
+                        <h2 style="color: #313541; font-weight: 700;">Preço</h2>
+                        <h1 style="color: #009447; font-weight: 700; margin-top: -10px;"><?= $b->preco; ?>ECV</h1>
+                    </div>
 
-                <div class="col-md-3 text-center"><!-- <br> --><br><br>
-                    <div class="fundo_circle">
-                        <div class="c100 p50">
-                            <span>68% <br>1500</span>
-                            <div class="slice">
-                                <div class="bar"></div>
-                                <div class="fill"></div>
+                    <div class="col-md-3 text-center"><!-- <br> --><br><br>
+                        <div class="fundo_circle">
+                            <div class="c100 p50">
+                                <span>68% <br>1500</span>
+                                <div class="slice">
+                                    <div class="bar"></div>
+                                    <div class="fill"></div>
+                                </div>
                             </div>
                         </div>
+                        <span style="color: #009447; font-weight: 700; text-transform: uppercase; font-size: 20px;">Entrada</span>
                     </div>
-                    <span style="color: #009447; font-weight: 700; text-transform: uppercase; font-size: 20px;">Entrada</span>
-                </div>
 
-                <div class="col-md-3 text-center"><!-- <br> --><br><br>
-                    <div class="fundo_circle">
-                        <div class="c100 p50">
-                            <span>68%<br><small>5000</small></span>
-                            <div class="slice">
-                                <div class="bar"></div>
-                                <div class="fill"></div>
+                    <div class="col-md-3 text-center"><!-- <br> --><br><br>
+                        <div class="fundo_circle">
+                            <div class="c100 p50">
+                                <span>68%<br><small>5000</small></span>
+                                <div class="slice">
+                                    <div class="bar"></div>
+                                    <div class="fill"></div>
+                                </div>
                             </div>
                         </div>
+                        <span style="color: #009447; font-weight: 700; text-transform: uppercase; font-size: 20px;">Stock</span>
                     </div>
-                    <span style="color: #009447; font-weight: 700; text-transform: uppercase; font-size: 20px;">Stock</span>
                 </div>
             </div>
-        </div>
+        <?php endforeach; ?>
     <!-- .tab-content -->
     </div>
 
--- backend/views/evento/view.php
+++ backend/views/evento/view.php
@@ -3,36 +3,15 @@ use yii\helpers\Html;
 use yii\widgets\DetailView;
 ?>
 
-<div class=""style="padding: 0px 55px;margin:0px;">
+<div class="" style="padding: 0px 55px;margin:0px;">
     <div class="container-fluid info_evento_evento">
-        <h4 ><div class="borderlefttitlo"></div><span style="margin-left: 20px; font-size: 20px; color: #009447; font-weight: 700; font-family: 'DINBold'; text-transform: uppercase;">Info Evento</span></h4>
-        <br>
-        <div class="row">
-            <div class="col-md-12">
-                <div class="destail_evento">
-                    <h1><?= $model->nome; ?></h1>
-                    <span>
-                        <div class="assetpf icon-date"></div>
-                        <i class="fa fa-calendar" style="margin-right: 10px;"></i>
-                        <?= date( 'd',strtotime($model->data) ) //dia?>
-                        <?= Yii::$app->mycomponent->MesExtencoNome(date( 'm', strtotime($model->data) )) //mes?>
-                        de <?= date( 'Y', strtotime($model->data) ) //ano?>
-                    </span><br>
-                    <span>
-                    <div class="assetpf icon-local"></div>
-                    <i class="fa fa-map-marker" style="font-size: 30px; margin-right: 10px; margin-top: 5px;"></i> <?=$model->local; //Local?>,
-                        <?= $model->ilha //ilha?></span><br><br>
-                    <div class="descricao_text" style="width: 50%;color:#fff;">
-                        <p><?= $model->descricao?></p>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <h4><div class="borderlefttitlo"></div><span style="margin-left: 20px; font-size: 20px; color: #009447; font-weight: 700; font-family: 'DINBold'; text-transform: uppercase;">Info Evento</span></h4>
+        <?php echo $this->render('evento_info', ['model' => $model]); ?>
     </div>
-    <!-- evento info -->
+
     <div class="container-fluid info_evento_evento">
-        <h4 ><div class="borderlefttitlo"></div><span style="margin-left: 20px; font-size: 20px; color: #009447; font-weight: 700; font-family: 'DINBold'; text-transform: uppercase;">Info Bilhetes</span></h4>
-        <?php echo $this->render('evento_bilhetes', []); ?>
+        <h4><div class="borderlefttitlo"></div><span style="margin-left: 20px; font-size: 20px; color: #009447; font-weight: 700; font-family: 'DINBold'; text-transform: uppercase;">Info Bilhetes</span></h4>
+        <?php echo $this->render('evento_bilhetes', ['_dataBilhetes' => $_dataBilhetes]); ?>
     </div>
 </div>
 
