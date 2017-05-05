95ebb011044df7c982ff3cde0671e63e279fe432
Ayrton Gomes <com.ayrton@gmail.com>
2017-05-05 06:53:00 -0100

    more stuff for this

--- backend/controllers/AnalyticsController.php
+++ backend/controllers/AnalyticsController.php
@@ -1,6 +1,8 @@
 <?php
 namespace backend\controllers;
+
 use backend\models\UploadForm;
+use backend\models\Business;
 use backend\models\analytics\ReportsService;
 use backend\models\analytics\AnalyticsService;
 use backend\components\RestApp;
@@ -15,7 +17,7 @@ class AnalyticsController extends \yii\web\Controller {
         $session = \Yii::$app->session;
 
         if ($user->can('business')) {
-            $_GET['business_id'] = $session->get('business');
+            $bizId = $_GET['business_id'] = $session->get('business');
         } else if ($user->can('producer')) {
             $_GET['producer_id'] = Produtor::find()->where(['idprodutor'=>$user->id])->one()->marca_idmarca;
         }
