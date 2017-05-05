a70d4812e1721217a406a977462c95b7468a9a43
Ayrton Gomes <com.ayrton@gmail.com>
2017-05-05 01:50:42 -0100

    working on stuff

--- backend/controllers/AnalyticsController.php
+++ backend/controllers/AnalyticsController.php
@@ -13,17 +13,15 @@ class AnalyticsController extends \yii\web\Controller {
     public function actionApi() {
         $user = \Yii::$app->user;
         $session = \Yii::$app->session;
-        $filters = RestApp::parseQueryFilters($_GET);
 
         if ($user->can('business')) {
-            $filters['business_id'] = $session->get('business');
+            $_GET['business_id'] = $session->get('business');
         } else if ($user->can('producer')) {
-            $filters['producer_id'] = Produtor::find()->where(['idprodutor'=>$user->id])->one()->marca_idmarca;
+            $_GET['producer_id'] = Produtor::find()->where(['idprodutor'=>$user->id])->one()->marca_idmarca;
         }
 
-
+        $filters = RestApp::parseQueryFilters($_GET);
         $service = new AnalyticsService();
-
         $data = $service->getGlobalReport($filters);
         $data['business_data'] = $service->getBusinessReport($filters);
         $data['producer_data'] = $service->getProducerReport($filters);
