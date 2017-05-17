1f14290d9702066b8057c997793abb5c0a004d02
Ayrton Gomes <com.ayrton@gmail.com>
2017-05-17 13:38:15 -0100

    start faq module

---
+++ backend/models/Faq.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace backend\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "faqs".
+ *
+ * @property integer $id
+ * @property string $pergunta
+ * @property string $resposta
+ * @property integer $estado
+ */
+class Faq extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'faqs';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['pergunta', 'resposta', 'estado'], 'required'],
+            [['pergunta', 'resposta'], 'string'],
+            [['estado'], 'integer']
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'pergunta' => 'Pergunta',
+            'resposta' => 'Resposta',
+            'estado' => 'Estado',
+        ];
+    }
+}
---
+++ backend/views/faq/_form.php
@@ -0,0 +1,27 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model backend\models\Faq */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="faq-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'pergunta')->textarea(['rows' => 6]) ?>
+
+    <?= $form->field($model, 'resposta')->textarea(['rows' => 6]) ?>
+
+    <?= $form->field($model, 'estado')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
