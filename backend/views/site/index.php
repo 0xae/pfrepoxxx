eeaaa3c5db00b21887260e25da454a2a6bb4fe28
Janito Vieira <janitovv04@gmail.com>
2017-05-18 17:44:45 -0100

    janito custom pass md5

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -6,7 +6,7 @@ $user = Yii::$app->user;
 
 ?>
 
-<div class="container-fluid pagebusiness">
+<div class="container-fluid pagebusiness overview-page">
     <?php /* echo $this->render('business_modal', []); */ ?>
     <?php echo $this->render('staff_dashboard', []); ?>
 </div>
