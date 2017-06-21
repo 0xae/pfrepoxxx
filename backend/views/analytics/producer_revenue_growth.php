89eec167ecc744eec654ab248f87373c7db214f9
Ayrton Gomes <com.ayrton@gmail.com>
2017-06-21 04:30:48 -0100

    frontend is ready

--- backend/views/analytics/producer_revenue_growth.php
+++ backend/views/analytics/producer_revenue_growth.php
@@ -2,7 +2,7 @@
     <div class="row" style="padding:15px" >
         <div class="col-md-10">
             <center>
-                <h4>Gross Revenue Per Producer</h4>
+                <h4>Revenue growth</h4>
             </center>
             <div ng-if="no_sellers_data" style="margin-bottom:-18em;margin-top:10em;">
                 <no-data></no-data>
